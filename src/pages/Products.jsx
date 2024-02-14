f6cef9e3b372ddab13012274c1a4e503ad16a321
mdsajalali <mdsajalali2020@gmail.com>
2024-02-14 12:24:31 +0600

    category added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -18,7 +18,19 @@ const Products = () => {
       <h1 className="text-3xl font-semibold pb-1 border-[#59B210] border-b-[3px]   w-[120px] mx-auto">
         Products
       </h1>
+
       <Container>
+        <div className="flex justify-end items-center mt-5">
+          <select className="select select-bordered w-full max-w-xs">
+            <option disabled selected>
+              Choose one
+            </option>
+            <option>Camera</option>
+            <option>Phone</option>
+            <option>Laptop</option>
+            <option>Headphone</option>
+          </select>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 mb-20">
           {products.map((product, id) => (
             <div key={id} className="bg-white rounded-lg shadow-md p-4">
