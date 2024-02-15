e945155c0b5fda0cbafd6c3c1e7c8925dd8ec1f9
mdsajalali <mdsajalali2020@gmail.com>
2024-02-15 19:31:47 +0600

    brands position changed

--- src/pages/TopBrands.jsx
+++ src/pages/TopBrands.jsx
@@ -1,11 +1,15 @@
 import Marquee from "react-fast-marquee";
-import Container from "../components/Container";
 import img1 from "../assets/images/brands/brand-01.jpg";
-import img2 from "../assets/images/brands/brand-02.jpg";
-import img3 from "../assets/images/brands/brand-03.jpg";
+import {
+  default as img2,
+  default as img5,
+} from "../assets/images/brands/brand-02.jpg";
+import {
+  default as img3,
+  default as img6,
+} from "../assets/images/brands/brand-03.jpg";
 import img4 from "../assets/images/brands/brand-04.jpg";
-import img5 from "../assets/images/brands/brand-02.jpg";
-import img6 from "../assets/images/brands/brand-03.jpg";
+import Container from "../components/Container";
 
 const images = [img1, img2, img3, img4, img5, img6];
 
@@ -13,7 +17,7 @@ const TopBrands = () => {
   return (
     <Container>
       <div className="my-10">
-        <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-500">
+        <h1 className="text-3xl font-semibold pb-1 border-[#59B210] border-b-[3px]   md:text-3xl   mb-10 text-gray-500    w-[152px] mx-auto">
           Top Brands
         </h1>
         <Marquee>
