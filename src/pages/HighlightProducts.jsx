6880a885bbd487ace859b67f30793a246728717e
mdsajalali <mdsajalali2020@gmail.com>
2024-02-16 20:44:34 +0600

    Highlight Products data updated

--- src/pages/HighlightProducts.jsx
+++ src/pages/HighlightProducts.jsx
@@ -1,4 +1,12 @@
 import feaImg1 from "../assets/images/highlights/product-small-01.jpg";
+import feaImg2 from "../assets/images/highlights/product-small-02.jpg";
+import feaImg3 from "../assets/images/highlights/product-small-03.jpg";
+import feaImg4 from "../assets/images/highlights/product-small-04.jpg";
+import feaImg5 from "../assets/images/highlights/product-small-05.jpg";
+import feaImg6 from "../assets/images/highlights/product-small-06.jpg";
+import feaImg7 from "../assets/images/highlights/product-small-07.jpg";
+import feaImg8 from "../assets/images/highlights/product-small-08.jpg";
+import feaImg9 from "../assets/images/highlights/product-small-09.jpg";
 import Container from "../components/Container";
 import Newsletter from "../components/Newsletter";
 
@@ -12,63 +20,63 @@ const FeaturedProductsData = [
   },
   {
     id: 2,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "PowerShot Elph 115 16MP Digital Camera.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg2,
   },
   {
     id: 3,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "PowerShot Elph 115 16MP Digital Camera.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg3,
   },
 ];
 const OnSaleProductsData = [
   {
     id: 1,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "HP Scanner 2910P",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg4,
   },
   {
     id: 2,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "Galaxy Tab 3 GT-P5210 16GB, Wi-Fi, 10.1in.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg5,
   },
   {
     id: 3,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "PowerShot Elph 115 16MP Digital Camera.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg6,
   },
 ];
 const TopRatedProductsData = [
   {
     id: 1,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "PowerShot Elph 115 16MP Digital Camera.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg7,
   },
   {
     id: 2,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "PowerShot Elph 115 16MP Digital Camera.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg8,
   },
   {
     id: 3,
-    heading: "Netbook Acer Travel B113-E-10072.",
+    heading: "Surface RT 64GB, Wi-Fi, 10.6in - Dark Titanium.",
     regular_price: "$1873",
     old_price: "$2000",
-    img: feaImg1,
+    img: feaImg9,
   },
 ];
 
@@ -77,13 +85,16 @@ const HighlightProducts = () => {
     <>
       <div className="bg-[#F9F9F9] border-t border-b">
         <Container>
-          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-10 pt-14 pb-20">
+          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3   gap-10 pt-14 pb-20">
             <div>
               <h1 className="text-2xl font-semibold text-[#59B210] border-b pb-2">
                 Featured Products
               </h1>
               {FeaturedProductsData.map((product) => (
-                <div key={product.id} className="flex items-center gap-5 my-5 ">
+                <div
+                  key={product.id}
+                  className="flex items-center justify-between gap-5 my-5 "
+                >
                   <div>
                     <h1 className="text-[#0E53A5] hover:text-[#59B210] transition-all cursor-pointer font-semibold">
                       {product.heading}
@@ -104,7 +115,10 @@ const HighlightProducts = () => {
                 On-Sale Products
               </h1>
               {OnSaleProductsData.map((product) => (
-                <div key={product.id} className="flex items-center gap-5 my-5 ">
+                <div
+                  key={product.id}
+                  className="flex items-center justify-between gap-5 my-5 "
+                >
                   <div>
                     <h1 className="text-[#0E53A5] hover:text-[#59B210] transition-all cursor-pointer font-semibold">
                       {product.heading}
@@ -125,7 +139,10 @@ const HighlightProducts = () => {
                 Top Rated Products
               </h1>
               {TopRatedProductsData.map((product) => (
-                <div key={product.id} className="flex items-center gap-5 my-5 ">
+                <div
+                  key={product.id}
+                  className="flex items-center justify-between gap-5 my-5 "
+                >
                   <div>
                     <h1 className="text-[#0E53A5] hover:text-[#59B210] transition-all cursor-pointer font-semibold">
                       {product.heading}
