ce38051dd815b5862abaf5d6b8f5c6bbb66ddd24
mdsajalali <mdsajalali2020@gmail.com>
2024-02-17 18:42:50 +0600

    hero section responsive isses fixed

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,11 +5,11 @@ import Container from "../components/Container";
 const Hero = () => {
   return (
     <Container>
-      <div className="grid grid-cols-12 gap-10">
-        <div className="lg:col-span-4 col-span-12">
+      <div className="grid grid-cols-1 md:grid-cols-12 lg:gap-10">
+        <div className="lg:col-span-4 md:col-span-5 col-span-12">
           <Category />
         </div>
-        <div className="lg:col-span-8 col-span-12">
+        <div className="lg:col-span-8 md:col-span-7 col-span-12">
           <Banner />
         </div>
       </div>
