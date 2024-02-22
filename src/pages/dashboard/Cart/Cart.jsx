c15334a5ad87a7f0158788676c80291c860c36cf
mdsajalali <mdsajalali2020@gmail.com>
2024-02-22 17:42:41 +0600

    cart table functionality added

--- src/pages/dashboard/Cart/Cart.jsx
+++ src/pages/dashboard/Cart/Cart.jsx
@@ -1,13 +1,41 @@
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "./../../../hooks/useCart";
 const Cart = () => {
-  const [cart] = useCart();
-  console.log(cart);
+  const [cart, refetch] = useCart();
+  // console.log(cart);
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div className="my-10">
       <div className="flex items-center justify-evenly gap-10 ">
-        <h1 className="uppercase text-2xl">Total Order: </h1>
-        <h2 className="uppercase text-2xl">Total Price: $</h2>
+        <h1 className="uppercase text-2xl">Total Order: {cart.length} </h1>
+        <h2 className="uppercase text-2xl">Total Price: ${totalPrice}</h2>
 
         <button className="flex items-center  gap-2 bg-[#59B210]  px-5 py-2 text-white  hover:bg-[#0E53A5] transition-all rounded-md">
           Pay
@@ -47,10 +75,13 @@ const Cart = () => {
                     <div className="text-sm opacity-50">{item.rating}</div>
                   </div>
                 </td>
-                <td>{item.price}</td>
+                <td>${item.price}</td>
                 <td>{item.brand}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-xs"
+                  >
                     <FaTrashAlt />
                   </button>
                 </th>
